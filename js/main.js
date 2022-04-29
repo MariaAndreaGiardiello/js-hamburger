279523a7c4602e5d20acf08f1de5bf5927004ba8
MariaAndreaGiardiello <giardiello.m.a@icloud.com>
2022-04-29 16:34:20 +0200

    analisi problema

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 // Crea visualizzazione menu a tendinda
 // Componenti menu: Home, Scopri il corso, la nostra storia, iscriviti
 
+// 1. Creare classe per definire lo spazio in cui racchiudere il menu
+// 2. Modificare visualizzazione hamnurger-menu al click dell'utente
+// 3. Definire elemento a cui attribuire la funzione click
+// 4. Eseguire funzione click
+// 5. Visualizzare menu-hamburger solo in schermi <1000px
